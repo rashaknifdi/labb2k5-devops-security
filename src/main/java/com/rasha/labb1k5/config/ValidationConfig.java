80775aae45f046b2bd6c5a812e0c25e5a8083574
rasha knifdi <rasha.knifdi@chasacademy.se>
2026-06-30 01:55:33 +0200

    Add Validator bean to fix Spring context initialization in tests

---
+++ src/main/java/com/rasha/labb1k5/config/ValidationConfig.java
@@ -0,0 +1,17 @@
+package com.rasha.labb1k5.config;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ValidationConfig {
+
+    @Bean
+    public Validator validator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        return factory.getValidator();
+    }
+}
